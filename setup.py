e723ee27a250d187153fd0afbcc73023b5339db6
sheva <olegbeznogy@gmail.com>
2025-01-13 09:19:05 +0300

    doc

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="mymra", 
-    version="1.4.9.8",  
+    version="1.4.9.9",  
     packages=find_packages(include=["mymra"]),
     install_requires=[
         'pycryptodome',
     ],
     description="A tool for embedding and extracting files and strings with AES encryption.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'Oleg Beznogy',
     author_email = 'olegbeznogy@gmail.com',
     url="https://github.com/moshiax/mymra",
